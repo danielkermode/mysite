522f267df9d4740ba8a73480840075a07a46f129
danielkermode <danielkermode@hotmail.co.nz>
2016-09-20 00:47:40 +1200

    Good to go

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ if [ "$2" = "fast" ]
 then
   docker run --env-file secrets/Dockercreds dockercloud/cli service redeploy $serviceId
 else
+  git add .
+  git commit -m "$1"
+  git push origin master
   docker build -t absurdisms/daniel:latest .
   docker push absurdisms/daniel:latest
   docker run --env-file secrets/Dockercreds dockercloud/cli service redeploy $serviceId
